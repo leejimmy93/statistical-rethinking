799256a742a928fcf00ad1914b7c24501d87eabe
Ruijuan Li <rzl0007@auburn.edu>
2016-05-23 17:01:31 -0700

    small change

--- code_R_club_book.R
+++ code_R_club_book.R
@@ -1684,24 +1684,28 @@ mu <- apply(pred.m6.14, 2, mean)
 mu.PI <- apply(pred.m6.14, 2, PI)
 mu
 length(mu)
+mu.PI
 
 # plot it all
 plot(kcal.per.g ~ neocortex, d, col=rangi2)
 lines(nc.seq, mu, lty=2)
 lines(nc.seq, mu.PI[1,], lty=2) # x and y lengths differ??? 
 lines(nc.seq, mu.PI[2,], lty=2)
-
+shade(mu.PI, nc.seq)
 mu.PI
 
 # R code 6.30
 milk.ensemble <- ensemble(m6.11, m6.12, m6.13, m6.14, data = d.predict)
 ?ensemble # use link & sim for a list of map model fit to construct Akaike weighted ensemble of predictions
+dim(milk.ensemble$link)
+head(milk.ensemble$link)
 
 mu <- apply(milk.ensemble$link, 2, PI)
 mu.PI < apply(milk.ensemble$link, 2, PI)
 lines(nc.seq, mu)
 shade(mu.PI, nc.seq)
 
+# markdown file output options: ... cache=TRUE, dependency on the previous chunk of code... 
 
 
 
