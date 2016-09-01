95082698eb3a34f70bac3a2342d5f60cc5972451
Ruijuan Li <rzl0007@auburn.edu>
2016-08-31 18:59:42 -0700

    part of chapter 10 R code

--- code_R_club_book.R
+++ code_R_club_book.R
@@ -2356,6 +2356,143 @@ max(entropies)
 distribution[which.max(entropies)]
 ?which.max
 
+########################## chapter 10 
+# R code 10.1 
+library(rethinking)
+data("chimpanzees")
+d <- chimpanzees
+
+?chimpanzees
+# R code 10.2 
+m10.1 <- map(
+  alist(
+    pulled_left ~ dbinom(1, p),
+    logit(p) <- a, 
+    a ~ dnorm(0, 10)
+  ), 
+  data = d)
+precis(m10.1)
+
+# R code 10.3
+logistic(c(0.18, 0.46))
+
+# R code 10.4 
+m10.2 <- map(
+  alist(
+    pulled_left ~ dbinom(1, p), 
+    logit(p) <- a + bp*prosoc_left, 
+    a ~ dnorm(0, 10), 
+    bp ~ dnorm(0, 10)
+  ), 
+  data = d)
+
+m10.3 <- map(
+  alist(
+    pulled_left ~ dbinom(1, p), 
+    logit(p) <- a + (bp + bpC*condition)*prosoc_left,
+    a ~ dnorm(0, 10), 
+    bp ~ dnorm(0, 10), 
+    bpC ~ dnorm(0, 10)
+  ), 
+  data = d)
+
+# R code 10.5 
+compare(m10.1, m10.2, m10.3)
+
+# R code 10.6
+precis(m10.3)
+
+# R code 10.7 
+exp(0.61)
+
+# R code 10.8 
+logistic(4)
+
+# R code 10.9 
+logistic(4 + 0.61)
+
+# R code 10.10 
+# dummy data for predictions across treatments
+d.pred <- data.frame(
+  prosoc_left = c(0,1,0,1), # right/left/right/left
+  condition = c(0,0,1,1) # control/control/partner/partner
+)
+
+# build prediction ensemble 
+chimp.ensemble <- ensemble(m10.1, m10.2, m10.3, data = d.pred)
+?ensemble
+class(chimp.ensemble)
+head(chimp.ensemble$link) # four outcomes for 4 different combinations  
+
+# summarize 
+pred.p <- apply(chimp.ensemble$link, 2, mean)
+pred.p.PI <- apply(chimp.ensemble$link, 2, PI)
+
+pred.p
+pred.p.PI
+
+# R code 10.11
+# empty plot frame with good axes
+plot(0, 0, type="n", xlab="prosoc_left/condition",
+     ylab="proportion pulled left", ylim=c(0,1), xaxt="n",
+     xlim=c(1,4))
+axis(1, at=1:4, labels = c("0/0", "1/0", "0/1", "1/1"))
+
+# plot raw data, one trend for each of 7 indivividual chimpanzees
+# will use by() here; see Overthinking box for explanation
+p <- by(d$pulled_left, 
+        list(d$prosoc_left, d$condition, d$actor), mean)
+
+by(d$pulled_left, 
+   list(d$prosoc_left, d$condition, d$actor), mean)
+
+for(chimp in 1:7)
+  lines(1:4, as.vector(p[,,chimp]), col=rangi2, lwd=1.5)
+
+# now superimpose posterior prediction 
+lines(1:4, pred.p)
+shade(pred.p.PI, 1:4)
+
+# R code 10.12
+# clean NAs from the data
+d2 <- d
+d2$recipient <- NULL
+
+# re-use map fit to get the formula
+m10.3stan <- map2stan(m10.3, data = d2, iter = 1e4, warmup = 1000)
+precis(m10.3stan)
+
+# R code 10.13
+pairs(m10.3stan)
+
+# R code 10.14 
+m10.4 <- map2stan(
+  alist(
+    pulled_left ~ dbinom(1, p),
+    logit(p) <- a[actor] + (bp + bpC*condition)*prosoc_left,
+    a[actor] ~ dnorm(0, 10),
+    bp ~ dnorm(0, 10),
+    bpC ~ dnorm(0, 10)
+  ), 
+  data = d2, chains = 2, iter = 2500, warmup = 500)
+
+# R code 10.15 
+unique(d$actor)
+
+# R code 10.16 
+precis(m10.4, depth = 2)
+
+# R code 10.17 
+post <- extract.samples(m10.4)
+str(post) # why I got 400 rows??? 
+
+
+
+
+
+
+
+
 
 
 
